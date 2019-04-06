a70fd3497e5182131d29447b2a4d99f46272b826
pavel <s20305382@gmail.com>
2019-04-06 23:33:17 +0300

    nitial commit

---
+++ src/ajaxGet.jsx
@@ -0,0 +1,31 @@
+
+
+export default function (url, constant, params){
+    const tokens = constant.split('_');
+    let key = '';
+    if(tokens.length === 0){
+        console.error('Undefined token');
+        return {}
+    }else if(tokens.length === 1){
+        key = tokens[0];
+    }else{
+        for(let i=0; i<tokens.length-1;i++){
+            key += tokens[i]+'_';
+        }
+    }
+    return (dispatch, getStore) => {
+        dispatch({type:key+'REQ', payload:{params:params}});
+
+        $.get(url, params, (r) => {
+            if(r.status === 'ok'){
+                dispatch({type:key+'DONE', payload:{params:params, response:r}});
+            }else{
+                dispatch({type:key+'ERR', payload:{params:params, response:r.msg?r.msg:r}});
+            }
+        }, 'json')
+            .fail((e) => {
+                console.error(url, key, e);
+                dispatch({type:key+'ERR', payload:{params:params, response:e.responseText}});
+            });
+    };
+}
